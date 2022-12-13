9e354d45e07fd7b95432ef8586ff00db0c1a201a
resilienciaeth <106503220+resilienciaeth@users.noreply.github.com>
2022-12-12 19:33:19 -0500

    left

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -8,7 +8,7 @@ function Cards() {
     <div className="relative mt-10">
       <div className="mt-20 relative  mb-20">
         <Image src={images.card} objectFit="fill" className="opacity-50" />
-        <div className="absolute md:top-1/2 md:left-1/2 md:w-full md:-translate-x-1/2 md:-translate-y-1/2 top-[30%]">
+        <div className="absolute md:top-1/2 md:left-1/2 md:w-full md:-translate-x-1/2 md:-translate-y-1/2 top-[30%] left-[30%] ">
           <p className="text-center text-[12px] nm:text-[40px] font-nesatho text-libera-2">LIBERA FEMINA</p>
           <h1 className="font-nesatho px-4 text-[18px] nm:text-[40px] text-center text-white">
             Cada pieza representa todas
